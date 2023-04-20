1acaf8986a1cdde75cab460b1cf9105d5636be9e
Mayank Singh <117936126+mayank4singh@users.noreply.github.com>
2023-04-20 07:20:53 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
 
         btn0 = findViewById(R.id.Btn0);
         btn1 = findViewById(R.id.Btn1);
